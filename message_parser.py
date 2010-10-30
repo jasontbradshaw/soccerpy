d31d89ef89c88b5c2645b7502ebac705caa68a59
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2010-10-30 18:51:12 -0500

    added string parsing to message_parser, it will now correctly handle strings containing '(' and ')'. cleaned up message_parser.py

--- message_parser.py
+++ message_parser.py
@@ -2,14 +2,20 @@
 
 def parse(text):
     """
-    Here is what amounts to a simple lisp parser for parsing the server's
+    This is what amounts to a simple lisp parser for turning the server's
     returned messages into an intermediate format that's easier to deal
     with than the raw (often poorly formatted) text.
+    
+    This parses generally, taking any lisp-like string and turning it into a
+    list of nested lists, where each nesting (besides the outermost) indicates
+    a parenthesized expression.  The outermost list is simply a container for
+    holding multiple top-level parenthesized expressions. Ex: "(foo 1) (bar 2)"
+    becomes [['foo', '1'], ['bar', '2']].
     """
     
     # make sure all of our parenthesis match
     if text.count("(") != text.count(")"):
-        raise ValueError("Text has some unmatching parenthesis!")
+        raise ValueError("Text has unmatching parenthesis!")
     
     # result acts as a stack that holds the strings grouped by nested parens
     result = []
@@ -21,49 +27,71 @@ def parse(text):
     # we indent or dedent, and then are added to the current indent level all
     # at once, for efficiency.
     s = []
+    
+    # whether we're currently in the middle of parsing a string
+    in_string = False
+    
+    # the last character seen, None to begin with
+    prev_c = None
     for c in text:
-        if c == "(":
-            s = []
+        # prevent parsing parens when inside a string (also ignores escaped
+        # '"'s as well).
+        if c == '"' and prev_c != "\\":
+            in_string = not in_string
+            s.append(c)
+        
+        # we only indent/dedent if not in the middle of parsing a string
+        elif c == "(" and not in_string:
             # find current level of nesting
             cur = result
             for i in xrange(indent):
                 cur = cur[-1]
-                
-            # add our buffered string onto the previous level
+            
+            # add our buffered string onto the previous level, then clear it
+            # for the next.
             if len(s) > 0:
                 cur.append(''.join(s))
                 s = []
             
-            # append a new level of nesting to it
+            # append a new level of nesting to our list
             cur.append([])
             
             # increase the indent level so we can get back to this level
             indent += 1
-        elif c == ")":
+        
+        elif c == ")" and not in_string:
             # append remaining string buffer before dedenting
             if len(s) > 0:
                 cur = result
                 for i in xrange(indent):
                     cur = cur[-1]
                     
-                # append our buffered string to the results
+                # append our buffered string to the last level, then clear it
                 cur.append(''.join(s))
                 s = []
             
             # we finished with one level, so dedent back to the previous one
             indent -= 1
+    
+        # append non-space characters to the buffer list. spaces are delimiters
+        # for expressions, hence are special.
         elif c != " ":
-            # append the current string character to the buffer list
+            # append the current string character to the buffer list.
             s.append(c)
+        
+        # we separate expressions by spaces
         elif c == " " and len(s) > 0:
             cur = result
             for i in xrange(indent):
                 cur = cur[-1]
-                
+            
             # append our buffered string to the results
             cur.append(''.join(s))
             s = []
-
+        
+        # save the previous character. used to determine if c is escaped
+        prev_c = c
+    
     return result
 
 if __name__ == "__main__":
