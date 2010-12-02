87b80a533482b0840ab2a12cfe7464634ba05836
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2010-12-02 01:05:32 -0600

    changed agent run script to launch each agent as a seperate physical process. this allows us to sidestep the GIL and get decent processing speeds when running an entire team simultaneously

--- agent.py
+++ agent.py
@@ -276,46 +276,59 @@ class Agent:
 
 if __name__ == "__main__":
     import sys
+    import multiprocessing as mp
 
     # enforce corrent number of arguments, print help otherwise
     if len(sys.argv) < 3:
         print "args: ./agent.py <team_name> <num_players>"
         sys.exit()
 
-    # arg1: team name
-    # arg2: number of players to create
+    def spawn_agent(team_name):
+        """
+        Used to run an agent in a seperate physical process.
+        """
 
-    agentlist = []
-    total_agents = 0
-    for agent in xrange(min(11, int(sys.argv[2]))):
-        print "Creating Agent %d:" % agent
-
-        print "  Initializing...",
         a = Agent()
-        print "\t[DONE]"
-
-        print "  Connecting...",
-        a.connect("localhost", 6000, sys.argv[1])
-        print "\t[DONE]"
-
-        print "  Starting play...",
+        a.connect("localhost", 6000, team_name)
         a.play()
-        print "\t[DONE]"
 
-        agentlist.append(a)
-        total_agents += 1
+        # we wait until we're killed
+        while 1:
+            # we sleep for a good while since we can only exit if terminated.
+            time.sleep(1)
 
+    # spawn all agents as seperate processes for maximum processing efficiency
+    agentthreads = []
+    for agent in xrange(min(11, int(sys.argv[2]))):
+        print "  Spawning agent %d..." % agent
+
+        at = mp.Process(target=spawn_agent, args=(sys.argv[1],))
+        at.daemon = True
+        at.start()
+
+        agentthreads.append(at)
+
+    print "Spawned %d agents." % len(agentthreads)
     print
-    print "Launched %d agents." % total_agents
+    print "Playing soccer..."
 
+    # wait until killed to terminate agent processes
     try:
         while 1:
             time.sleep(0.05)
     except KeyboardInterrupt:
-        print "Disconnecting agents..."
-        for agent in agentlist:
-            print "  Disconnecting...",
-            agent.disconnect()
-            print "\t[DONE]"
-        print "Agents disconnected."
+        print
+        print "Killing agent threads..."
+
+        # terminate all agent processes
+        count = 0
+        for at in agentthreads:
+            print "  Terminating agent %d..." % count
+            at.terminate()
+            count += 1
+        print "Killed %d agent threads." % (count - 1)
+
+        print
+        print "Exiting."
+        sys.exit()
 
